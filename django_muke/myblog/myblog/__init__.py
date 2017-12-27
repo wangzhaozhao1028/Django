edde7df7f0c6d672efd81b8d8363655eb10b4661
t-wangzhaozhao <t-wangzhaozhao@megvii.com>
2017-12-27 12:23:43 +0800

    muke

---
+++ django_muke/myblog/myblog/__init__.py
@@ -0,0 +1,3 @@
+#coding=utf-8
+# 声明文件  https://www.imooc.com/video/13934
+# 视频来自 django入门与实践  慕课网
---
+++ django_muke/myblog/myblog/settings.py
@@ -0,0 +1,92 @@
+#coding=utf-8
+"""
+Django settings for myblog project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+#项目根目录
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+#安全码
+SECRET_KEY = 'u@kp4@3%1go2yej9k416kksi8tigqs@h=ria%r*(cmo=k9@0&!'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+#调试,不要在生产环境中开启,因为错误会抛给前端
+DEBUG = True
+# DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+# 前端的提示:
+# You're seeing this message because you have DEBUG = True in your Django settings file and you haven't configured any URLs. Get to work!
+ALLOWED_HOSTS = []
+
+
+# Application definition
+#自己的应用要填加进去
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+# url的根文件
+ROOT_URLCONF = 'myblog.urls'
+
+WSGI_APPLICATION = 'myblog.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+#语言
+LANGUAGE_CODE = 'en-us'
+# 时区
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# 静态文件的地址
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
